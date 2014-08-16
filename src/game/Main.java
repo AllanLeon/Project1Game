4f706fc29fb942288a11259695b1a62be00c14aa
Allan Leon <allan.leon@outlook.com>
2014-08-16 17:34:29 -0400

    Implemented keyListener and paint methods

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,6 +1,10 @@
 package game;
 
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
@@ -9,12 +13,13 @@ import javax.swing.JFrame;
  * Main window of the game
  * @author Allan Leon
  */
-public class Main extends JFrame {
+public class Main extends JFrame implements KeyListener {
 	
 	private final int WIDTH = 800;
 	private final int HEIGHT = 480;
 
 	private BufferedImage doubleBuffer;
+	private Insets borders;
 
 	/**
 	 * Launch the application.
@@ -40,15 +45,69 @@ public class Main extends JFrame {
 	}
 	
 	/**
-	 * Initializes and set ups the basic components of the frame.
+	 * Initialize and set up the basic components of the frame.
 	 */
 	private void initialize() {
+		borders = getInsets();
+		
 		setTitle("Game");
-		setSize(WIDTH, HEIGHT);
+		setSize(borders.left + WIDTH + borders.right,
+				borders.top + HEIGHT + borders.bottom);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
+		
 		doubleBuffer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 	}
 
+	/**
+	 * Copy the doubleBuffer image to the main graphics.
+	 */
+	@Override
+	public void paint(Graphics g) {
+        g.drawImage(doubleBuffer, borders.left, borders.top, this); 
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void keyPressed(KeyEvent ke) {
+		switch (ke.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			break;
+		case KeyEvent.VK_DOWN:
+			break;
+		case KeyEvent.VK_W:
+			break;
+		case KeyEvent.VK_S:
+			break;
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void keyReleased(KeyEvent ke) {
+		switch (ke.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			break;
+		case KeyEvent.VK_DOWN:
+			break;
+		case KeyEvent.VK_W:
+			break;
+		case KeyEvent.VK_S:
+			break;
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void keyTyped(KeyEvent ke) {
+		// TODO Auto-generated method stub
+		
+	}
 }
