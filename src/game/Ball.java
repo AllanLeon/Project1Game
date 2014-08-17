35d37f1ef2a37309ad78cd5a1be9b770bd0b4126
Allan Leon <allan.leon@outlook.com>
2014-08-17 18:56:50 -0400

    Added getters and setters

--- src/game/Ball.java
+++ src/game/Ball.java
@@ -1,11 +1,8 @@
 package game;
 
-import java.awt.geom.Ellipse2D;
-
 public class Ball {
 
 	private int centerX, centerY, speedX, speedY, radius;
-	private Ellipse2D bounds;
 	
 	public Ball(int centerX, int centerY, int radius) {
 		this.centerX = centerX;
@@ -13,6 +10,50 @@ public class Ball {
 		this.speedX = 0;
 		this.speedY = 0;
 		this.radius = radius;
-		bounds = new Ellipse2D.Double(centerX, centerY, radius, radius);
+	}
+	
+	public void update() {
+		centerX += speedX;
+		centerY += speedY;
+	}
+
+	public int getCenterX() {
+		return centerX;
+	}
+
+	public int getCenterY() {
+		return centerY;
+	}
+
+	public int getSpeedX() {
+		return speedX;
+	}
+
+	public int getSpeedY() {
+		return speedY;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setCenterX(int centerX) {
+		this.centerX = centerX;
+	}
+
+	public void setCenterY(int centerY) {
+		this.centerY = centerY;
+	}
+
+	public void setSpeedX(int speedX) {
+		this.speedX = speedX;
+	}
+
+	public void setSpeedY(int speedY) {
+		this.speedY = speedY;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
 	}
 }
--- src/game/Ship.java
+++ src/game/Ship.java
@@ -1,19 +1,9 @@
 package game;
 
-import java.awt.Rectangle;
 
-public class Ship {
-
-	private int x, y, speedX, speedY, width, height;
-	private Rectangle bounds;
+public class Ship extends BasicRectangle {
 	
 	public Ship(int x, int y, int width, int height) {
-		this.x = x;
-		this.y = y;
-		this.speedX = 0;
-		this.speedY = 0;
-		this.width = width;
-		this.height = height;
-		bounds = new Rectangle(x, y, width, height);
+		super(x, y, width, height);
 	}
 }
